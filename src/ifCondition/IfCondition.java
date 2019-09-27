def8f8e2479bc863a4d7fc38cb1303e570f5dac2
Adit Choudhary <hug46663@adobe.com>
2019-09-27 15:34:36 +0530

    initial Commit

---
+++ src/ifCondition/IfCondition.java
@@ -0,0 +1,35 @@
+package ifCondition;
+
+public class IfCondition {
+boolean a;
+
+
+	public static void main(String[] args) {
+		boolean a;
+		//The local variable a may not have been initialized
+		//System.out.println(a);
+
+		//Addding Comment just to check
+		
+		/*Adding this line in branch2 and
+		trying to make this branch on remote */
+		
+		IfCondition condition = new IfCondition();
+		System.out.println(condition.a);
+		
+		
+		
+		if(a=true){
+		System.out.println("In if");
+		}
+		else{
+			System.out.println("In else");
+		}
+		System.out.println(a= true);
+		int i;
+		
+		System.out.println(i=2);
+		
+	}
+
+}
---
+++ src/singleton/SingletonSerializationTest.java
@@ -0,0 +1,32 @@
+package singleton;
+
+import serialization.SerializationUtil;
+
+public class SingletonSerializationTest {
+
+    public static void main(String[] args) {
+
+        LazyLoadingSingletonWithSerialization obj1 =
+            LazyLoadingSingletonWithSerialization.getInstance();
+
+        LazyLoadingSingletonWithSerialization obj2 =
+            LazyLoadingSingletonWithSerialization.getInstance();
+        try {
+            obj2.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+
+        String filename = "singleton";
+
+        SerializationUtil.Serialize(obj1, filename);
+        System.out.println("obj1-> " + obj1.hashCode());
+        System.out.println("obj2-> " + obj2.hashCode());
+
+        LazyLoadingSingletonWithSerialization obj1new = (LazyLoadingSingletonWithSerialization) SerializationUtil.deserialize(filename);
+        System.out.println("obj1new -> " + obj1new.hashCode());
+
+    }
+
+
+}
